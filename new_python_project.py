07408c2f702ebc141f27ec921651ed8d3466650e
idonacmias <idonacmi@gmail.com>
2022-11-30 23:59:44 +0200

    create_venv func: remove value project_name because lack of use

--- new_python_project.py
+++ new_python_project.py
@@ -8,11 +8,13 @@ import click
 def open_project(project_name, src):
 	create_new_folder(project_name)
 	os.chdir(project_name)
-	create_venv(project_name)
+	create_venv()
 	create_commen_files(project_name)
 	git_init_to_push(project_name, src)	
+	activate_venv():
+	install_requirements()
 
-def create_venv(project_name):
+def create_venv():
 	os.system(f'python3 -m venv myenv')
 
 def create_new_folder(project_name):
@@ -29,13 +31,19 @@ def create_file(file_name, text=''):
 
 def git_init_to_push(project_name, src):
 	cmds = ['git init',	
-   		    'git add .',
+		    'git add .',
 		    'git commit . -m "first comment"',
 		   f'git remote add origin {src}',
 		    'git push -u origin master']
 	
 	for cmd in cmds:
    		os.system(cmd) 
+
+def activate_venv():
+	pass
+
+def install_requirements():
+	pass
 
 if __name__ == '__main__':
 	open_project()
