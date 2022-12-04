8e24b9f58abe96d6f3545c8d06a148ebebf7790f
idonacmias <idonacmi@gmail.com>
2022-12-04 10:39:15 +0200

    split git_init_to_push to list generetore and list_commend func, def acive_venv and install_requirements, install_requirements still need generalization, load_config is now warking

--- new_python_project.py
+++ new_python_project.py
@@ -1,21 +1,38 @@
 import os
-import click
+# import click
+from sys import platform #to defirate between linux and windows
+import fire
+from json import load
 
-@click.command()
-@click.option('--project_name', prompt='project name')
-@click.option('--src', prompt='git repository link',
-			help="conect new git reposetory by open one in github and copy it here")
-def open_project(project_name, src):
+# @click.command()
+# @click.option('-n', '--project_name')
+# @click.option('--src', default='wwwwwwww', prompt='git repository link',
+# 			help="conect new git reposetory by open one in github and copy it here")
+
+def open_project(project_name):
+	config = load_config()
 	create_new_folder(project_name)
 	os.chdir(project_name)
 	create_venv()
 	create_commen_files(project_name)
-	git_init_to_push(project_name, src)	
-	activate_venv():
+	src = generate_src(project_name, config["git_user_name"])
+	git_init_to_push(project_name, src) 
 	install_requirements()
 
+def load_config():
+	config_file = main_folder() + '/config.json' 
+	with open(config_file, 'r') as f:
+		config = load(f)
+
+	return config
+
+def main_folder():
+	split_file = __file__.split('/')
+	main_folder = '/'.join(split_file[:-1])
+	return main_folder
+
 def create_venv():
-	os.system(f'python3 -m venv myenv')
+	os.system(f'python3 -m virtualenv myenv')
 
 def create_new_folder(project_name):
 	project_path = os.path.join(os.getcwd(), project_name)
@@ -29,22 +46,42 @@ def create_file(file_name, text=''):
 	with open(file_name, 'a') as f:
 		f.write(f'{text}')
 
-def git_init_to_push(project_name, src):
-	cmds = ['git init',	
-		    'git add .',
-		    'git commit . -m "first comment"',
-		   f'git remote add origin {src}',
-		    'git push -u origin master']
-	
-	for cmd in cmds:
-   		os.system(cmd) 
 
-def activate_venv():
-	pass
+def generate_src(project_name, git_user_name):
+	src = f'git@github.com:{git_user_name}/{project_name}.git'
+	return src
+
+def git_init_to_push(project_name, src):
+	git_commends = ['git init', 
+			'git add .',
+			'git commit . -m "first comment"',
+		   f'git remote add origin {src}',
+			'git push -u origin master']
+	join_comends(git_commends)
+
+def join_comends(commends):
+	new_commend = '&& '.join(commends)
+	print(f'new_commend: {new_commend}')
+	os.system(new_commend)
+
+
+def activate_venv_commend():
+	return '. myenv/bin/activate'
 
 def install_requirements():
-	pass
+	if platform == 'linux':
+		pip = 'pip3'
+
+	else:
+		pip = 'pip'
+
+	requirements = [f'{pip} install flask',
+					f'{pip} freeze > requirements.txt',
+					'git commit requirements.txt -m "install requirements"',
+					'git push -u origin']
+	requirements.insert(0, activate_venv_commend())
+	join_comends(requirements)
 
 if __name__ == '__main__':
-	open_project()
+	fire.Fire(open_project)
 	
